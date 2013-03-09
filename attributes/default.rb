52ca15cbd3b3674ca512d95572cc226d9303fa51
David Brodsky <davidpbrodsky@gmail.com>
2013-03-08 18:12:10 -0800

    temp work. todo: move ssl stuff into separate recipe. install libjpeg and pil properly. run as service

--- attributes/default.rb
+++ attributes/default.rb
@@ -17,6 +17,7 @@ default['ow_python']['ssl_databag_item_name'] 	= "ssl"
 
 # System
 default['ow_python']['app_root']      		= "/var/www/ReopenWatch"
+default['ow_python']['domain']      		= "chef.openwatch.net"
 default['ow_python']['git_user']      		= "git"
 default['ow_python']['service_user']      	= "python"
 default['ow_python']['service_user_gid']    = 500
@@ -26,7 +27,8 @@ default['ow_python']['git_url']      		= "git@github.com:OpenWatch/ReopenWatch.g
 default['ow_python']['git_rev']      		= "HEAD"
 default['ow_python']['git_branch']      	= "v2" # Can't get this working yet
 default['ow_python']['git_ssh_wrapper']   	= "/home/git/.ssh/wrappers/ow-github_deploy_wrapper.sh"
-default['ow_python']['log_path']		    = "/var/log/ow_python.log"
+default['ow_python']['log_dir']     		= "/var/log/ow/"
+default['ow_python']['service_log']			= "ow_python.log"
 
 #Django
 default['ow_python']['local_settings_file'] = "local_settings.py.erb"
@@ -35,6 +37,7 @@ default['ow_python']['node_api_user']	    = "test"
 default['ow_python']['etherpad_url']		= "http://pad.openwatch.net:9001/api"
 default['ow_python']['stripe_publishable']	= "pk_test_R5RY0ez5odUzDnzjCbno32jf"
 default['ow_python']['aws_bucket_name']		= "openwatch-static"
+default['ow_python']['internal_port']		= 8000
 
 #Postgres
 default['ow_python']['db_host']	    		= "localhost"
@@ -49,9 +52,8 @@ default['ow_python']['https_listen_port']   = 443
 default['ow_python']['ssl_dir']				= "/srv/ssl/"
 default['ow_python']['ssl_cert']     		= "star_openwatch_net.crt"
 default['ow_python']['ssl_key']     		= "star_openwatch_net.key"
-default['ow_python']['log_dir']     		= "/var/log/ow/"
-default['ow_python']['access_log']     		= "nginx_access_python.log"
-default['ow_python']['error_log']     		= "nginx_error_python.log"
+default['ow_python']['access_log']     		= "ow_python_nginx_access.log"
+default['ow_python']['error_log']     		= "ow_python_nginx_error.log"
 default['ow_python']['proxy_pass']     		= "http://localhost:8000"
 
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -28,9 +28,9 @@ application node['ow_python']['service_name'] do
   repository node['ow_python']['git_url']
   revision node['ow_python']['git_rev']
   deploy_key ssh_key['id_rsa']
+  # THIS SYMLINKS INTO THE APP ROOT. WHYYYYYYY!
   symlink_before_migrate ({"local_settings.py" => "reopenwatch/reopenwatch/local_settings.py"})
-  symlinks ({"local_settings.py" => "reopenwatch/reopenwatch/local_settings.py"})
-  migrate true
+  migrate false
   # packages should be handled separately?
   # packages ["git", "git-core", "mercurial"]
 
@@ -55,6 +55,87 @@ application node['ow_python']['service_name'] do
     	:aws_bucket_name => node['ow_python']['aws_bucket_name'],
     })
     debug true
-    collectstatic true
+    collectstatic false
   end
+end
+
+# collectstatic + syncdb
+# TODO: get django resource to do this
+bash "collectstatic and syncdb" do
+  user node['ow_python']['git_user']
+  cwd node['ow_python']['app_root'] + '/current/reopenwatch'
+  code <<-EOH
+  /var/www/ReopenWatch/shared/env/bin/python manage.py collectstatic --noinput
+  /var/www/ReopenWatch/shared/env/bin/python manage.py syncdb --noinput
+  EOH
+end
+
+## TODO: Put in separate ow_server cookbook
+# Make directory for ssl credentials
+directory node['ow_python']['ssl_dir'] do
+  owner node['nginx']['user']
+  group node['nginx']['group']
+  recursive true
+  action :create
+end
+
+# SSL certificate and key
+cookbook_file node['ow_python']['ssl_dir'] + node['ow_python']['ssl_cert']  do
+  source "star_openwatch_net.crt"
+  owner node['nginx']['user']
+  group node['nginx']['group']
+  mode 0600
+  action :create
+end
+
+ssl_key = Chef::EncryptedDataBagItem.load(node['ow_python']['ssl_databag_name'] , node['ow_python']['ssl_databag_item_name'] )
+
+file node['ow_python']['ssl_dir'] + node['ow_python']['ssl_key'] do
+  owner node['nginx']['user']
+  group node['nginx']['group']
+  content ssl_key['*.openwatch.net']
+  mode 0600
+  action :create
+end
+
+# Make Nginx log dirs
+directory node['ow_python']['log_dir'] do
+  owner node['nginx']['user']
+  group node['nginx']['group']
+  recursive true
+  action :create
+end
+
+## END TODO: separate cookbook
+
+# Nginx config file
+template node['nginx']['dir'] + "/sites-enabled/ow_python.nginx" do
+    source "ow_python.nginx.erb"
+    owner node['nginx']['user']
+    group node['nginx']['group']
+    variables({
+    :http_listen_port => node['ow_python']['http_listen_port'],
+    :https_listen_port => node['ow_python']['https_listen_port'],
+    :app_domain => node[:fqdn],
+    :domain => node['ow_python']['domain'],
+    :internal_port => node['ow_python']['internal_port'],
+    :ssl_cert => node['ow_python']['ssl_dir'] + node['ow_python']['ssl_cert'],
+    :ssl_key => node['ow_python']['ssl_dir'] + node['ow_python']['ssl_key'],
+    :app_root => node['ow_python']['app_root'],
+    :access_log => node['ow_python']['log_dir'] + node['ow_python']['access_log'],
+    :error_log => node['ow_python']['log_dir'] + node['ow_python']['error_log'],
+    :proxy_pass => node['ow_python']['proxy_pass']
+    })
+    notifies :restart, "service[nginx]"
+    action :create
+end
+
+
+## XXX: Temp server run
+bash "collectstatic and syncdb" do
+  user node['ow_python']['git_user']
+  cwd node['ow_python']['app_root'] + '/current/reopenwatch'
+  code <<-EOH
+  /var/www/ReopenWatch/shared/env/bin/python manage.py runserver 0.0.0.0:8000
+  EOH
 end
